f92d1e8f2f270cf282ec7370878285ea67ef1327
pasindu-sandaruwan <pasindu.sliit32@gmail.com>
2023-06-04 00:30:13 +0530

    Integrate the controller and routes
    - Testing the API for a success image saving

--- controllers/cat.controller.js
+++ controllers/cat.controller.js
@@ -6,10 +6,30 @@ const getAndSaveMergedCatImages = async ( req, res, next ) =>{
         const firstMessage = req.query.firstMessage;
         const secondMessage = req.query.secondMessage;
 
-        const firstImage = await fetchCatImageWithText(firstMessage, null);
-        const secondImage = await fetchCatImageWithText(secondMessage, null );
+        // validate for the missing message inputs
+        if( !firstMessage || !secondMessage ){
+            return res.status(400).json({
+                message : "Both messages are required"
+            });
+        }
+
+        // set to default values if the query params are not available 
+        const width = req.query.width ?? 400;
+        const height = req.query.height ?? 500;
+        const color = req.query.color ?? "Pink";
+        const size = req.query.size ?? 100;
+
+        const params = {
+            width : width,
+            height : height,
+            c : color,
+            s : size
+        }
+
+        const firstImage = await fetchCatImageWithText(firstMessage, params);
+        const secondImage = await fetchCatImageWithText(secondMessage, params );
     
-        await mergeTwoImages( firstImage, secondImage, 400 );
+        await mergeTwoImages( firstImage, secondImage, width );
 
         return res.status(200).json({message : "Image saved successfully"});
 
