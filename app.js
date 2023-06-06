205820595848918200471bc8f2f73cf7d464f16a
pasindu-sandaruwan <pasindu.sliit32@gmail.com>
2023-06-07 03:06:58 +0530

    Refactor common util and app.js
    -Refactor the common util to ES8
    - Call the functions and test for output in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
+import catService from './services/cat.service.js';
 import constants from './utils/common.constants.js';
 import minimist from 'minimist';
+import commonUtil from './utils/common.util.js';
 
 const argv = minimist(process.argv.slice(2));
 const argVInputsArray =  argv._; //store the argv input array to a variable
@@ -29,8 +31,26 @@ const prepareAndGetImageProps = ( inputs ) =>{
 }
 
 const main = async () =>{
+    const {firstMessage, secondMessage} = prepareAndGetImageInputs( argVInputsArray );
+    const imageProps = prepareAndGetImageProps( argVInputsArray );
 
+    // Put the API calls into a Promise Array
+    const apiCalls = [ 
+        catService.fetchCatImageWithText(firstMessage, imageProps ),
+        catService.fetchCatImageWithText(secondMessage, imageProps )
+    ];
 
+    // call the Promise all get the responses at once
+    const images = await Promise.all(apiCalls)
+        .then(responses=>{
+            return responses;
+        })
+        .catch(err=>{
+            console.error(err);
+            throw new Error(err);
+        });
+    const mergedImage = await commonUtil.mergeTwoImages( images[0], images[1]);
+    await commonUtil.saveImageToStorage(mergedImage);
 }
 
 main();
